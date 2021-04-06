1823901db2f3cf2f51f4aaedecf039f8a4172912
unknown <anupraj15@gmail.com>
2021-04-06 16:17:15 +0530

    amazon to git repo push

---
+++ src/test/java/Amazon/Search.java
@@ -0,0 +1,22 @@
+package Amazon;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Search {
+	
+public static void main(String args[])
+{
+	
+	System.setProperty("webdriver.chrome.driver","D:\\Anup\\Automation testing\\software\\chromedriver_win321\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.amazon.in/");
+	
+	WebElement text = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+	text.sendKeys("Realme Mobile");
+    driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+
+}
+}
